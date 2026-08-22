bdaeb4fee0ca75c552b86e38b5cefad5123bcc71
Saumya010907 <mauryasaumya251@gmail.com>
2026-08-22 23:33:38 +0530

    Add files via upload

---
+++ basics.cpp
@@ -0,0 +1,15 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"My name is Saumya";
+//     cout<<"\n";                 
+//     cout<<endl;
+//     cout<<"my age is 19";
+// }
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x=5,y =4;
+    cout<<x*y;
+}
