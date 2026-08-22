c6d8dc94ad36e8c904b54146d4b212c0d32a8244
Saumya010907 <mauryasaumya251@gmail.com>
2026-08-22 23:38:18 +0530

    Add files via upload

---
+++ halfarray.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[]={1,2,3,4,5,6,7,8};
+    int size=8;
+    cout<<"Half of array:";
+    for(int i=0;i<size/2;i++){
+        cout<<arr[i]<<" ";
+    }
+ 
+return 0;
+}
